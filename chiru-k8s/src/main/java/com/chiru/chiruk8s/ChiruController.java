a13a68cbfb41f61f140f97cc064f7e960f2e5fe0
chiranjeevi36 <rchiranjeevi36@gmail.com>
2022-06-29 14:31:48 +0000

    updated new commits

--- chiru-k8s/src/main/java/com/chiru/chiruk8s/ChiruController.java
+++ chiru-k8s/src/main/java/com/chiru/chiruk8s/ChiruController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class JhooqController {
+public class ChiruController {
 
     @GetMapping("/hello")
     public String hello() {
