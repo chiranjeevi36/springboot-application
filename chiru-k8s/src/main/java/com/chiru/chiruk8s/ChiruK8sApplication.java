ae0f30a75d17e741fb51feba20b96e5bdc525ffc
CHIRANJEEVI RACHAPUDI <79960625+chiranjeevi36@users.noreply.github.com>
2022-06-29 13:38:47 +0530

    Add files via upload

---
+++ chiru-k8s/src/main/java/com/chiru/chiruk8s/ChiruK8sApplication.java
@@ -0,0 +1,13 @@
+package com.chiru.chiruk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiruK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiruK8sApplication.class, args);
+	}
+
+}
---
+++ chiru-k8s/src/test/java/com/chiru/chiruk8s/ChiruK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiru.chiruk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiruK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
